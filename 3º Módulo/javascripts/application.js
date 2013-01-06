54193347694c8758006dfa225cf589c00b8c7bb8
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-01-05 21:29:37 -0600

    applyed changes of first module to all the submodules

--- 3º Módulo/javascripts/application.js
+++ 3º Módulo/javascripts/application.js
@@ -37,8 +37,10 @@ $(document).ready(function() {
 function check(form)/*function to check userid & password*/
 {
   /*the following code checkes whether the entered userid and password are matching*/
-  if(form.userid.value == "test" && form.pswrd.value == "12345"){
+  if(form.userid.value == "padre" && form.pswrd.value == "12345"){
     window.open('main.html','_self');/*opens the target page while Id & password matches*/
+  }else if(form.userid.value == "admin" && form.pswrd.value == "12345"){
+    window.open('main_admin.html','_self');/*opens the target page while Id & password matches*/
   }else{
     alert("Verifique sus datos");/*displays error message*/
   }
